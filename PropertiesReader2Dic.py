0c2b5ab6d377f03cab5c634d0d2655b0063728e4
ZackLaskar <Muzakkir@192.168.1.14>
2017-03-19 17:54:18 +0000

    Modified comments in PropertiesReader2Dic.py..

--- PropertiesReader2Dic.py
+++ PropertiesReader2Dic.py
@@ -1,7 +1,7 @@
 # Wrote this code to read a custom property file into a dictionary.
 # Jython does not have that cool configParser module as Python to read, parse the properties file.
-# This code reads values of a particular section of the properties file into a dictionary.
-# Usage: PropFileReader function should be passed as an argumen, the property file and the section to be read from it.
+# This code reads values of a particular section in the properties file into a dictionary.
+# Usage: PropFileReader function should be passed as an argument, the property file and the section to be read from it.
 # 	 Eg: Refer Test2Dict.properties for example of the property file to be used for this code.
 
 
